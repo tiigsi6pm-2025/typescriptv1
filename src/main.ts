ff2a5b077455f06b5647615aac71ae222a877d78
Abdiasis-Mustafe <tullirmustafe@gmail.com>
2025-04-09 09:35:51 +0300

    updated lesson2

--- src/main.ts
+++ src/main.ts
@@ -44,4 +44,47 @@ interface student{
 }
 
 
-let students:{ name:string ,phone:number,grade:number} ={ name:"yusuf",phone:64635,grade:8,}
+let students:{ name:string ,phone:number,grade?:number} ={ name:"yusuf",phone:64635, grade:8}
+
+interface std{
+    name:string,
+    phone:number,
+    gender?:boolean
+}
+let arday:std ={name:"yacquub",phone:5783737}
+
+type emp={
+    name:string,
+    phone:string|number,
+    title:string|null
+}
+  let shql:emp= { name:"kaafiya",phone:463783 ,title:"" }
+
+//partial 
+let mng: Partial<emp>={name:"ysuuf",phone:"74733"}
+
+// read only   
+const api: Readonly<{url:string}> ={url:"api route"}
+//pick
+type mrk= Pick<emp,"name"|"phone">
+
+const test:mrk={name:"chdfkjh",phone:9383663}
+//omit
+type deletOne= Omit<emp,"name">
+
+
+const userOne:deletOne={phone:464632,title:"heloo"}
+
+// functions
+function Greating(Name:string):string{
+ return ` hello ${Name}`
+}
+Greating("ysusu")
+
+let Q= function(){
+
+}
+
+let add:(a:number,b:number)=>number = (a,b)=>{
+ return a+b
+}
